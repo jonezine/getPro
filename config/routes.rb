0e9b924fc1342d8d3d553f635798f573136d597c
jonezine <jonezine@msn.com>
2013-11-27 17:40:35 +0000

    Modeling Users, News e Companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Getpro::Application.routes.draw do
+  resources :news
+
+  resources :companies
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
