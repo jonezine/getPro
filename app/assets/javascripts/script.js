1fb6310ac9e14ba1bd5238c3a2a2856b940fc988
jonezine <jonezine@msn.com>
2013-12-02 07:25:52 +0000

    Contact and Companies Links. Small changes.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,16 +3,16 @@
  */
 $(document).ready( function(){
 
-    /*Alterar background da pagina welcome*/
+    /*Alterar background da pagina welcome e animaçao*/
     var welcome = $('#welcometext');
+    welcome.parent().parent().css( 'background-image', 'url("/assets/welcomeimage.jpg")');
+    welcome.parent().parent().css( 'background-size', '110% auto');
     welcome.parent().parent().css( 'height', '450px');
-    welcome.parent().parent().css( 'background', 'url("/assets/welcomeimage.jpg")');
     welcome.hide().fadeIn(1500);
 
     /*Accao do botao login*/
     $('#loginform').hide();
     $('#login').click( function(){
-        $('')
         $('#loginform').slideDown('fast');
         $(this).css("text-align","center") ;
 
@@ -20,6 +20,12 @@ $(document).ready( function(){
     $('#login').mouseleave( function(){
         $('#loginform').slideUp('fast');
     });
+
+    /*Texto Notice ex:User password failed*/
+    var notice = $('.notice');
+    var alert = $('.alert');
+    notice.css("color","green");
+    alert.css("color","red");
 
 
 });
